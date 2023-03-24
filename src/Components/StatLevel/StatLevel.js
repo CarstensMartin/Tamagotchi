b12fd8daa476b6ac5bc2d3afdb99b65efc0bc1a3
sango <Matyila2sango@gmail.com>
2023-03-24 11:02:33 +0200

    [update]: background color

--- src/Components/StatLevel/StatLevel.js
+++ src/Components/StatLevel/StatLevel.js
@@ -3,13 +3,8 @@ import React, { useEffect } from 'react'
 
 export default function FeedBar({title, level, color}) {
 
-    // useEffect(() => {
-    //     const feedbar = document.getElementById("stat-level")
-    //     feedbar.style = `height: ${level}%`      
-    // }, [level])
-
     return (
-        <div className='stat-level' id='stat-level'>
+        <div className='stat-level' id='stat-level' style={{backgroundColor: color}}>
             <div className="stat-level-title-wrapper">
                 <span id="stat-level-title">{title}</span>
             </div>
